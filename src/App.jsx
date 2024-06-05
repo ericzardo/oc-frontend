7040ac0f2c5f62506ad54c986166e7e78620f546
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-06-05 03:58:50 -0300

    create: chatbox component basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import ChatBox from "./components/ChatBox"
+
 function App() {
-  return <h1 className="text-3xl font-bold text-zinc-100 text-center">Hello World!</h1> 
+  return (
+    <div className="w-auto mx-auto max-w-screen-lg bg-zinc-300 flex flex-1 flex-wrap justify-between p-12 rounded-t-2xl shadow-md">
+      <ChatBox />
+      <ChatBox />
+
+    </div>
+    
+  ) 
 }
 
 export default App
---
+++ src/components/ChatBox/index.jsx
@@ -0,0 +1,16 @@
+function ChatBox() {
+  return (
+    <span className="flex flex-col gap-5 p-5 w-64 h-28 shadow-md border-t border-blue-500 bg-zinc-200 rounded-lg cursor-pointer transition-transform hover:scale-110 ease-linear duration-100">
+
+      <h2 className="font-semibold text-2xl text-zinc-800">[chat-name]</h2>
+
+      <span className="flex justify-between text-blue-500 text-base text-">
+        <p>[online_users]</p>
+        <p>[created_at]</p>
+      </span>
+    </span>
+    
+  ) 
+}
+
+export default ChatBox
