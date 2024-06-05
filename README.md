d5f01aa1743d2802ba99d7e97e4d2c0dde2a894f
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-06-05 00:12:28 -0300

    setup project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Front-end for my Online Chat application
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1 className="text-3xl font-bold text-zinc-100 text-center">Hello World!</h1> 
+}
+
+export default App
