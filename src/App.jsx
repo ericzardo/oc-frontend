261a4312a9497fbe20a317f8e2a029e828239b62
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-06-05 19:51:16 -0300

    create: join chat popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import ChatBox from "./components/ChatBox"
+import { useState } from "react";
+
+import ChatBox from "./components/ChatBox";
+import JoinChatAlert from "./components/JoinChatAlert";
 
 function App() {
+  const [isAlertVisible, setAlertVisible ] = useState(false);
+
   return (
     <div className="w-auto mx-auto max-w-screen-lg bg-zinc-300 flex flex-1 flex-wrap justify-between p-12 rounded-t-2xl shadow-md">
       <ChatBox />
       <ChatBox />
+
+      {isAlertVisible && <JoinChatAlert onClose={() => setAlertVisible(false)}/>}
+
+      <JoinChatAlert />
 
     </div>
     
--- src/components/ChatBox/index.jsx
+++ src/components/ChatBox/index.jsx
@@ -1,6 +1,6 @@
 function ChatBox() {
   return (
-    <span className="flex flex-col gap-5 p-5 w-64 h-28 shadow-md border-t border-blue-500 bg-zinc-200 rounded-lg cursor-pointer transition-transform hover:scale-110 ease-linear duration-100">
+    <span className="flex flex-col gap-5 p-5 w-64 h-28 shadow-md border-t-2 border-blue-500 bg-zinc-200 rounded-lg cursor-pointer transition-transform hover:scale-110 ease-linear duration-100">
 
       <h2 className="font-semibold text-2xl text-zinc-800">[chat-name]</h2>
 
@@ -13,4 +13,4 @@ function ChatBox() {
   ) 
 }
 
-export default ChatBox
+export default ChatBox;
