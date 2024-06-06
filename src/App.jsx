f07a572795db74ed6ea9a8e16b89a6ba1fa054f4
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-06-05 22:04:35 -0300

    update: litte changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import { useState } from "react";
 
-import ChatBox from "./components/ChatBox";
-import JoinChatAlert from "./components/JoinChatAlert";
+import ChatBox from "@components/ChatBox";
+import JoinChatAlert from "@components/JoinChatAlert";
 
-function App() {
-  const [isAlertVisible, setAlertVisible ] = useState(false);
+function App () {
+  const [isJoiningChat, setJoiningChat ] = useState(false);
 
   return (
     <div className="w-auto mx-auto max-w-screen-lg bg-zinc-300 flex flex-1 flex-wrap justify-between p-12 rounded-t-2xl shadow-md">
+      <ChatBox onClick={() => setJoiningChat(true)} />
       <ChatBox />
-      <ChatBox />
 
-      {isAlertVisible && <JoinChatAlert onClose={() => setAlertVisible(false)}/>}
-
-      <JoinChatAlert />
-
+      {isJoiningChat && <JoinChatAlert onClose={() => setJoiningChat(false)}/>}
     </div>
     
-  ) 
+  ); 
 }
 
-export default App
+export default App;
