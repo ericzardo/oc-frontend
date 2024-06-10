fc17ebbf4f15cee39dbb09f6a1d49c9636493b74
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-06-10 23:34:48 +0000

    create: axios api

---
+++ src/lib/axios.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:5173/";
+
+const api = axios.create({
+  baseURL,
+  withCredentials: true,
+  headers: { "Content-Type": "application/json" }
+})
+
+export default api;
