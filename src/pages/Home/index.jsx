273d5941296b5d7181459ac5f008d7e8245602be
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-06-13 19:59:16 +0000

    test: ErrorModal component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,12 +8,15 @@ import ErrorModal from "@components/modals/ErrorModal";
 import ThemeDropdown from "@components/ThemeDropdown";
 
 import data from "./data";
+import { useState } from "react";
 
 function Home () {
   // const { data, isLoading, isError } = useQuery({
   //   queryKey: ["chats"],
   //   queryFn: getChats,
   // });
+
+  const [isError, setIsError] = useState(true);
 
   return (
     <div className="w-auto mx-auto max-w-screen-lg bg-zinc-300 flex flex-1 flex-wrap justify-between rounded-t-2xl shadow-md overflow-hidden">
@@ -31,6 +34,11 @@ function Home () {
       ))}
 
       <JoinChatAlert onSubmit={() => console.log("submit")} />
+      <ErrorModal
+        isOpen={isError}
+        onClose={() => setIsError(false)}
+        errorMessage="Testing error message"
+      />
     </div>
   );
 }
