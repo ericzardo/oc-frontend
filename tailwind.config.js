f3edca3909ceb561b1f73dafad696889a5cf482b
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2024-06-13 20:00:33 +0000

    create: progress bar animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        progressBarShrink: {
+          '0%': { width: '100%' },
+          '100%': { width: '0%' },
+        },
+      },
+      animation: {
+        'progress-bar': 'progressBarShrink 5s linear',
+      },
+    },
   },
   plugins: [],
 };
